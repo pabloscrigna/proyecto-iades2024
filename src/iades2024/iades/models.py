3dc2437de208d3c4d9b854568f94d4e7b4d76373
Pablo Scrigna <pscrigna@gruposimpli.com>
2024-10-17 19:19:55 -0300

    cambios clase1610

--- src/iades2024/iades/models.py
+++ src/iades2024/iades/models.py
@@ -3,9 +3,20 @@ from django.db import models
 # Create your models here.
 
 
-class Curso(models.Model):
+class Cursos(models.Model):
 
     nombre = models.CharField(max_length=40)
     codigo = models.IntegerField()
 
+    def __str__(self):
+        return f"curso: {self.nombre} -- codigo: {self.codigo}"
 
+
+class Estudiantes(models.Model):
+
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    dni = models.CharField(max_length=8, unique=True, blank=True, null=False)
+
+    def __str__(self):
+        return f"Estudiantes -- nombre: {self.nombre} -- dni: {self.dni}"
