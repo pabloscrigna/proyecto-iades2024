f562a6f68805c7104a9b99555085bec76ad849c1
Pablo Scrigna <pscrigna@gruposimpli.com>
2024-10-18 23:49:47 -0300

    clase1810

--- src/iades2024/iades/views.py
+++ src/iades2024/iades/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+from iades.models import Cursos, Estudiantes
 
 
 def inicio(request):
@@ -11,16 +12,35 @@ def inicio(request):
 
 def cursos(request):
 
+    # Leer todos los objetos de una tabla
+    cursos = Cursos.objects.all()    # select * from Cursos
+
+    # Comandos del ORM
+    # Filtrar por un campo
+    cursos = Cursos.objects.filter(nombre="HTML")
+
+    print(f"Curso filtrado: {cursos}")
+
+    # Obtener un objeto unico -- un solo objeto
+    cursos = Cursos.objects.get(id=2)
+
+    print(f"Objeto unico {cursos.id}  {cursos.nombre}")
+
     context_cursos = {
-        "id" : 45,
-        "nombre" : "python",
-        "alumnos" : 45
+        "cursos_list": cursos
     }
 
     return render(request, "cursos.html", context_cursos)
 
 
 def estudiantes(request):
+
+    # Trae todos los registros de la tabla estudiantes
+    estudiantes = Estudiantes.objects.all()
+
+    estudiantes = Estudiantes.objects.filter(dni=342422456)
+
+    print(estudiantes)
 
     return HttpResponse("vista de estudiantes")
 
