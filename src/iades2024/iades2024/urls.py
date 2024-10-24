98666f8158033271ca4a7906008e75948fed1695
Pablo Scrigna <pscrigna@gruposimpli.com>
2024-10-23 22:06:16 -0300

    clase 23/10

--- src/iades2024/iades2024/urls.py
+++ src/iades2024/iades2024/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("iades/", include('iades.urls')),
+    path("instituto/", include('iades.urls')),
+    # path("appname/", include('appname.urls'))
 ]
