864b574d6ba584dd810759c6cc3516ba3c99cd1d
Pablo Scrigna <pscrigna@gruposimpli.com>
2024-11-02 22:55:12 -0300

    clase 01/11

--- src/iades2024/iades/forms.py
+++ src/iades2024/iades/forms.py
@@ -10,3 +10,10 @@ class FormularioCurso(forms.Form):
 class FormularioBusquedaCurso(forms.Form):
     nombre = forms.CharField(max_length=10)
     codigo = forms.IntegerField(max_value=10)
+
+
+class FormularioProfesor(forms.Form):
+    nombre = forms.CharField(max_length=40)
+    apellido = forms.CharField(max_length=40)
+    dni = forms.CharField(max_length=8)
+    legajo = forms.IntegerField()
